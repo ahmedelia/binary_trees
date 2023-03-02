b825b38c22449babab12a2b5c7cb51f123c33420
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-02 11:13:59 +0200

    init new node in left branch of tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,13 +5,19 @@
  * @parent: pointer to parent
  * @value: node data
  * Return: void
-*/
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = binary_tree_node(parent, value);
+	binary_tree_t *new;
 
-	if (new == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
+
+	new = binary_tree_node(parent, value);
+
+	if (new == NULL)
+		return (NULL);
+
 	if (parent->right == NULL)
 	{
 		parent->right = new;
@@ -21,5 +27,4 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new->right = parent->right;
 	parent->right = new;
 	return (new);
-
 }
