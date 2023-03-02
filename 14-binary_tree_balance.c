abeecccf58aa3ffcff6e90cb0ec9b3f89b80aa7f
Yuu <ahmed.elia2003@gmail.com>
2023-03-02 16:09:48 +0200

    fixing 14-2

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * bh - get the hieght of longest branch in binary tree
+ * @tree: pointer to the root of the tree
+ * Return: height of the binary tree
+ */
+
+size_t bh(const binary_tree_t *tree)
+{
+	size_t left_side;
+	size_t right_side;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	left_side = binary_tree_height(tree->left) + 1;
+	right_side = binary_tree_height(tree->right) + 1;
+
+	if (left_side > right_side)
+		return (left_side);
+	return (right_side);
+}
+/**
  * binary_tree_balance - get the hieght factor
  * @tree: pointer to the root of the tree
  * Return: height of the binary tree
@@ -16,7 +37,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		ans += 1;
 	if (tree->right != NULL)
 		ans -= 1;
-	ans += binary_tree_height(tree->left);
-	ans -= binary_tree_height(tree->right);
+	ans += bh(tree->left);
+	ans -= bh(tree->right);
+
 	return (ans);
 }
