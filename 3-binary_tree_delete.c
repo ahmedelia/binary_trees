ef2ce438e527c0c475183aa73b7f697f17ebd24d
Yuu <ahmed.elia2003@gmail.com>
2023-03-02 11:31:58 +0200

    from 3-5 tasks

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+
+/**
+ * delete_node - helper
+ * @node: node to delete
+ * Return: void
+*/
+void delete_node(binary_tree_t *node)
+{
+	if (node == NULL)
+		return;
+
+	delete_node(node->left);
+	delete_node(node->right);
+	free(node);
+}
+/**
+ * binary_tree_delete - create node
+ * @tree: pointer to parent
+ * Return: void
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	delete_node(tree);
+}
