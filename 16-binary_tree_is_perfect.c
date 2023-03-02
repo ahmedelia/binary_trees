d560a6781c8ba8e99787c98ccf7b7af316dd804d
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-02 18:25:57 +0200

    add is_perfect mechanism for binary tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,16 +8,23 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_tree;
-	int right_tree;
+	return perfect_tree(tree, height(tree), 0);
+}
 
+int perfect_tree(const binary_tree_t *tree, int depth, int level)
+{
 	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (depth == level + 1);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	return (perfect_tree(tree->left, depth, level + 1) && perfect_tree(tree->right, depth, level + 1));
+}
 
-	left_tree = binary_tree_height(tree->left);
-	right_tree = binary_tree_height(tree->right);
-
-	return (right_tree == left_tree);
+int height(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	return 1 + height(node->left);
 }
